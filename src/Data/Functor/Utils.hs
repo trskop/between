4acddb485f1f2694dc339b9677ae4aaaf356c15a
Peter Trško <peter.trsko@gmail.com>
2013-09-06 18:36:18 +0200

    Introducing new functions in Data.Functor.Utils
    (<<$>>) :: Functor f => (a -> b -> c) -> (a -> f b) -> a -> f c
    (<<$$>>) :: Functor f => (a -> f b) -> (a -> b -> c) -> a -> f c
    iso :: Functor f => (c -> d) -> (a -> b) -> (b -> f c) -> a -> f d
    lens :: Functor f => (r -> (a, a -> r)) -> (a -> f a) -> r -> f r

--- src/Data/Functor/Utils.hs
+++ src/Data/Functor/Utils.hs
@@ -22,10 +22,18 @@ module Data.Functor.Utils
     -- ** Infix variations on fmap
     , (<$>)
     , (<$$>)
+    , (<<$>>)
+    , (<<$$>>)
 
-    -- ** Utility functions
+    -- ** Apply inside functor
     , (<#>)
     , (<##>)
+
+    -- ** Lenses
+    --
+    -- $lens
+    , iso
+    , lens
     )
     where
 
@@ -35,6 +43,8 @@ import Data.Functor (Functor(..), (<$>))
 #else
 import Control.Applicative ((<$), (<$>))
 #endif
+
+import Data.Function.Between (between)
 
 
 -- | Flipped version of '<$>', the naming convention comes from
@@ -50,6 +60,19 @@ infixl 4 <$$>
 infixl 4 $>
 {-# INLINE ($>) #-}
 
+-- | Instead of @\\ x -> f x '<$>' g x@ this function allows to write
+-- @f '<<$>>' g@.
+(<<$>>) :: Functor f => (a -> b -> c) -> (a -> f b) -> a -> f c
+(f <<$>> g) x = f x `fmap` g x
+infix 4 <<$>>
+{-# INLINE (<<$>>) #-}
+
+-- | Flipped variant of '<<$>>'.
+(<<$$>>) :: Functor f => (a -> f b) -> (a -> b -> c) -> a -> f c
+(<<$$>>) = flip (<<$>>)
+infix 4 <<$$>>
+
+{-# INLINE (<<$$>>) #-}
 -- | Like @\\ x f -> f '<*>' 'pure' x@, but does not have 'Applicative'
 -- constraint. Flipped version of '<#>'.
 --
@@ -80,3 +103,59 @@ infixl 4 <##>
 f <#> x = ($ x) `fmap` f
 infixl 4 <#>
 {-# INLINE (<#>) #-}
+
+-- | Create /lens/ from @f@ and @g@ that form an isomorphism.
+--
+-- Defined as: @iso f g = fmap f \`between\` g@
+iso :: (Functor f) => (c -> d) -> (a -> b) -> (b -> f c) -> a -> f d
+iso = between . fmap
+{-# INLINE iso #-}
+
+-- | Construct /lens/ from @r -> (a, a -> r)@-style lens.
+lens :: Functor f => (r -> (a, a -> r)) -> (a -> f a) -> r -> f r
+lens getSetItem f = setItemIn <<$>> f . getItemFrom
+  where
+    getItemFrom = fst . getSetItem  -- :: r -> a
+    setItemIn = snd . getSetItem    -- :: r -> a -> r
+{-# INLINEABLE lens #-}
+
+-- $lens
+--
+-- For newtypes and data types with single constructor and single attribute it
+-- is possible to create lens-like function simply by writing:
+--
+-- > l = iso constructor selector
+--
+-- This is due to the fact that constructor and selector together form an
+-- isomorphism. The /lens/ package has a lot of sugar for isomorphisms and you
+-- should check it out if they come up in your code.
+--
+-- Example:
+--
+-- > data Foo a = Foo {fromFoo :: a}
+-- >
+-- > foo :: Functor f => (a -> f a) -> Foo a -> f (Foo a)
+-- > foo = iso Foo fromFoo
+--
+-- Not so long ago there was a time when people thought that lenses had type
+-- @r -> (a, a -> r)@, or variation on that. There is also a lot of old code
+-- that it uses it directly or indirectly via some libraries that internally
+-- use such definition of lenses. To create new-style /lens/ from this old
+-- style you can use 'lens' function:
+--
+-- > data Foo a = Foo
+-- >     { _foo1 :: Int
+-- >     , _foo2 :: a
+-- >     , _foo3 :: a
+-- >     , _foo4 :: a
+-- >     }
+-- >
+-- > foo1 :: Functor f => (Int -> f Int) -> Foo a -> f (Foo a)
+-- > foo1 = lens $ \ f@Foo{_foo1 = x} -> (x, \ x' -> f{_foo1 = x'})
+--
+-- In cases when you need to avoid TemplateHaskell (like on platforms that
+-- don't support Haskell byte compilation, yet) these functions might come
+-- handy.
+--
+-- See /lens/ <http://hackage.haskell.org/package/lens> package for details
+-- on current standard lenses.
