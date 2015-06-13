f56f10467544e9f2a652025c9172004fb62e9cb3
Peter Trško <peter.trsko@gmail.com>
2015-06-13 15:49:41 +0200

    Corrected copyright year range

--- src/Data/Function/Between/Lazy.hs
+++ src/Data/Function/Between/Lazy.hs
@@ -2,7 +2,7 @@
 -- |
 -- Module:       $HEADER$
 -- Description:  Lazy function combinator "between" and its variations.
--- Copyright:    (c) 2013, 2014 Peter Trsko
+-- Copyright:    (c) 2013-2015 Peter Trško
 -- License:      BSD3
 --
 -- Maintainer:   peter.trsko@gmail.com
