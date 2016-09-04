ff080f67382966ad0bec8340a2d8914e8eb65560
Peter Trško <peter.trsko@gmail.com>
2016-09-04 12:30:26 +0200

    Use exec when running stack to get rid of parent shell

--- tools/stack-ghc8.sh
+++ tools/stack-ghc8.sh
@@ -5,10 +5,11 @@
 # apt-get install cabal-install-1.24 ghc-8.0.1 ghc-8.0.1-prof \
 #   ghc-8.0.1-htmldocs ghc-8.0.1-dyn
 export PATH="/opt/ghc/8.0.1/bin:/opt/cabal/1.24/bin:${PATH}"
+#export PATH="/home/dogmat/.local/bin:${PATH}"
 
 readonly stackBin='/usr/bin/stack'
 
 # Stackage nighthly now uses GHC 8.0.1.
 readonly yamlFile='stack-nightly.yaml'
 
-"${stackBin}" --stack-yaml "${yamlFile}" "$@"
+exec "${stackBin}" --stack-yaml "${yamlFile}" "$@"
